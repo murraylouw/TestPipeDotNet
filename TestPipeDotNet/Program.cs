5aa73e599357ba188b4781179a061ee7b7ca68c6
murraylouw <Murray.Louw@des-igngroup.com>
2022-04-13 14:34:29 +0200

    Changed to .NET send first, then read.

--- TestPipeDotNet/Program.cs
+++ TestPipeDotNet/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.IO.Pipes;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TestPipeDotNet
@@ -12,13 +13,16 @@ namespace TestPipeDotNet
     {
         static void Main(string[] args)
         {
+            // Start this program first, then run the Python project: TestPipePython
+            // Based on example from: https://jonathonreinhart.com/posts/blog/2012/12/13/named-pipes-between-c-and-python/
+
             // Open the named pipe.
             var server = new NamedPipeServerStream("ik_pipe");
 
             Console.WriteLine("Waiting for connection...");
             server.WaitForConnection();
-
             Console.WriteLine("Connected.");
+
             var reader = new BinaryReader(server);
             var writer = new BinaryWriter(server);
 
@@ -26,29 +30,33 @@ namespace TestPipeDotNet
             {
                 try
                 {
+                    // Send
+                    string tcp_pose = " x y z R P Y ";
+                    string message_from_dotnet = string.Format("<tcp_pose>{0}<tcp_pose/>", tcp_pose);
+
+                    var buffer = Encoding.ASCII.GetBytes(message_from_dotnet);     // Get ASCII byte array     
+                    writer.Write((uint)buffer.Length);                // Write string length
+                    writer.Write(buffer);                              // Write string
+                    Console.WriteLine("Sent from .NET: \"{0}\"", message_from_dotnet);
+
+
                     // Receive
                     var received_str_length = (int)reader.ReadUInt32();            // Read string length
                     var received_string = new string(reader.ReadChars(received_str_length));    // Read string
 
                     Console.WriteLine("Received from Python: \"{0}\"", received_string);
+                    Console.WriteLine();
 
-
-                    // Send
-                    string tcp_pose = " x y z R P Y ";
-                    string message_from_dotnet = string.Format("<tcp_pose>{0}<tcp_pose/>", tcp_pose);
-
-                    var buffer = Encoding.ASCII.GetBytes(received_string);     // Get ASCII byte array     
-                    writer.Write((uint)buffer.Length);                // Write string length
-                    writer.Write(buffer);                              // Write string
-                    Console.WriteLine(".NET sent: \"{0}\"", message_from_dotnet);
+                    Thread.Sleep(2000); // Wait for 2 seconds
+                    
                 }
-                catch (EndOfStreamException)
+                catch (EndOfStreamException) // When client disconnects
                 {
-                    break;                    // When client disconnects
+                    Console.WriteLine("Client disconnected.");
+                    break;                    
                 }
             }
-
-            Console.WriteLine("Client disconnected.");
+            
             server.Close();
             server.Dispose();
         }
